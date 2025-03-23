7c7fb5b13563f41aedc52fa41d17f5aab9640dca
anshu171 <anshuman.sahoobca22-25@asbm.ac.in>
2025-03-23 20:20:33 +0530

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "examcraft-cache-v1";
+const FILES_TO_CACHE = [
+  "ExamCraft.html",
+  "Styleexamcraft.css",
+  "styleimportpage.css",
+  "stylexamcraft.css",
+  "manifest.json",
+  "icon-192x192.png",
+  "icon-512x512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
